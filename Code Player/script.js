4b93f021cf18200fb4f3be6ef51a860776ed83db
Daniel Arul <danielarul07@gmail.com>
2019-12-05 00:37:04 +0530

    Completed Code player

--- Code Player/script.js
+++ Code Player/script.js
@@ -1,45 +1,39 @@
-$('textarea').height($(window).height()-$('#topbar').height()-15);
-$('.panel').width(($(window).width()/2)-7);
+//The following code is used to set the proper width for the textareas and iframe.
+var numberOfActivePanels = 4 - $('.hidden').length;
+$(".panel").width(($(window).width() / numberOfActivePanels) - 10);
 
-//This is to make the output panel update adter every change
+//The following code sets the height of the textarea and iframe
+$('textarea').height($(window).height() - $('#topbar').height() - 15);
+$('#outputText').height($(window).height() - $('#topbar').height() - 15);
 
-$("textarea").on('change keyup paste',function(){
-
-
+//This is to make the output panel update after every type
+$("textarea").on('change keyup paste', function() {
   updateOutputPanel();
 });
 
-function updateOutputPanel(){
-  $('iframe').contents().find('html').html("<html><head><style type='text/css'>" +$('#cssText').val()+"</style></head><body>"+
- $("#htmlText").val()+"</body></html>");
 
-
+//The code that updates the output based on the user input to the panels
+function updateOutputPanel() {
+  $('iframe').contents().find('html').html("<html><head><style type='text/css'>" + $('#cssText').val() + "</style></head><body>" +
+    $("#htmlText").val() + "</body></html>");
   document.getElementById('outputText').contentWindow.eval($("#javascriptText").val());
-
-
 };
 
-$('.toggleButton').click(function(){
-
+//To hide or show the HTML
+$('.toggleButton').click(function() {
   var panelId;
-  if($(this).attr("id")=="htmlb"){
+  if ($(this).attr("id") == "htmlb") {
     var panelId = "htmlText";
-  }
-  else if($(this).attr("id")=="cssb"){
+  } else if ($(this).attr("id") == "cssb") {
     var panelId = "cssText";
-  }
-  else if($(this).attr("id")=="javascriptb"){
+  } else if ($(this).attr("id") == "javascriptb") {
     var panelId = "javascriptText";
-  }
-  else{
+  } else {
     var panelId = "outputText";
   }
-
-  $('#'+panelId).toggleClass("hidden");
-
-  var numberOfActivePanels=4-$('.hidden').length;
-
-  $(".panel").width(($(window).width()/numberOfActivePanels)-10);
-
+  
+  $('#' + panelId).toggleClass("hidden");
+  var numberOfActivePanels = 4 - $('.hidden').length;
+  $(".panel").width(($(window).width() / numberOfActivePanels) - 10);
   updateOutputPanel();
 })
